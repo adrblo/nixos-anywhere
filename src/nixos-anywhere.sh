2bfa5c0caa9927a6a7a26b9de003c6aed0be7e69
Adrian Block <adrian@adrblo.de>
2025-03-08 09:50:15 +0100

    Revert "use nix-copy-closure"
    This reverts commit f0563ed605ad2f1a4a076a702b575ab0c223abd4.

--- src/nixos-anywhere.sh
+++ src/nixos-anywhere.sh
@@ -351,7 +351,7 @@ parseArgs() {
   fi
 
   if [[ $substituteOnDestination == "y" ]]; then
-    nixCopyOptions+=("--use-substitutes")
+    nixCopyOptions+=("--substitute-on-destination")
   fi
 
   if [[ $vmTest == "n" ]] && [[ -z ${sshConnection} ]]; then
@@ -393,7 +393,7 @@ runSsh() {
 }
 
 nixCopy() {
-  NIX_SSHOPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $sshKeyDir/nixos-anywhere -vvv ${sshArgs[*]}" nix-copy-closure \
+  NIX_SSHOPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $sshKeyDir/nixos-anywhere -vvv ${sshArgs[*]}" nix copy \
     "${nixOptions[@]}" \
     "${nixCopyOptions[@]}" \
     "$@"
@@ -656,7 +656,7 @@ runDisko() {
     # We need to do a nix copy first because nix build doesn't have --no-check-sigs
     # Use ssh:// here to avoid https://github.com/NixOS/nix/issues/7359
     nixCopy --to "ssh://$sshConnection?$sshStoreSettings" "${flake}#${flakeAttr}.system.build.${diskoMode}Script" \
-      --include-outputs
+      --derivation --no-check-sigs
     # If we don't use ssh-ng here, we get `error: operation 'getFSAccessor' is not supported by store`
     diskoScript=$(
       nixBuild "${flake}#${flakeAttr}.system.build.${diskoMode}Script" \
@@ -678,7 +678,7 @@ nixosInstall() {
     # We need to do a nix copy first because nix build doesn't have --no-check-sigs
     # Use ssh:// here to avoid https://github.com/NixOS/nix/issues/7359
     nixCopy --to "ssh://$sshConnection?remote-store=local%3Froot=%2Fmnt&$sshStoreSettings" "${flake}#${flakeAttr}.system.build.toplevel" \
-      --include-outputs
+      --derivation --no-check-sigs
     # If we don't use ssh-ng here, we get `error: operation 'getFSAccessor' is not supported by store`
     nixosSystem=$(
       nixBuild "${flake}#${flakeAttr}.system.build.toplevel" \
